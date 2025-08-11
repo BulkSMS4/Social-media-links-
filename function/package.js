566ca4b5a93a0b8e3658d4c68da17246b09e4e7e
BulkSMS4 <xportbox@gmail.com>
2025-08-11 01:36:14 +0000

    Create package.js

---
+++ function/package.js
@@ -0,0 +1,22 @@
+{
+  "name": "functions",
+  "scripts": {
+    "lint": "eslint .",
+    "serve": "firebase emulators:start --only functions",
+    "shell": "firebase functions:shell",
+    "start": "npm run shell",
+    "deploy": "firebase deploy --only functions",
+    "logs": "firebase functions:log"
+  },
+  "engines": {
+    "node": "18"
+  },
+  "main": "index.js",
+  "dependencies": {
+    "firebase-functions": "^4.0.0"
+  },
+  "devDependencies": {
+    "eslint": "^8.0.0"
+  },
+  "private": true
+}
